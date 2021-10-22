368fec56722f694ffa347b253c0d458553fa23a3
Mateus Santana <mateusssantana@gmail.com>
2021-10-22 19:37:41 -0300

    feature: laravel sail alias

--- mateussantana.plugin.zsh
+++ mateussantana.plugin.zsh
@@ -21,3 +21,9 @@ alias dln="docker network list"
 alias drcomposer='docker run -it --rm -u$(id -u):$(id -g) -v$PWD:/app -v${COMPOSER_HOME:-$HOME/.composer}:/tmp composer '
 alias drphp7='docker run -it --rm -u$(id -u):$(id -g) -v$PWD:/var/www/html php:7.4-fpm '
 alias drphp8='docker run -it --rm -u$(id -u):$(id -g) -v$PWD:/var/www/html php:8-fpm '
+
+alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli'
+
+# Laravel Sail
+alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
+
